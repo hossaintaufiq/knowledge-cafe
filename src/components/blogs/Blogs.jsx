1d572a104fc7431325fd6c91ff0d05f7dbf72279
Hossain Ahmmed <hossainahmmedtaufiq22@gmail.com>
2023-09-15 19:53:02 +0600

    ready project

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,5 +1,13 @@
+import { useEffect, useState } from "react";
 
 const Blogs = () => {
+
+    const [blogs,setBlogs]=useState([]);
+
+    useEffect(()=>{
+        
+    },[])
+
     return (
         <div>
             
